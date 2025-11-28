59c3f825f80c033a3b777a88f86d99774981d9d4
Deba00407 <cdebanjan456@gmail.com>
2025-11-28 11:45:06 +0530

    Testing Git Logs

--- File-2-MVC/src/main/java/com/debanjan/File2MvcApplication.java
+++ File-2-MVC/src/main/java/com/debanjan/File2MvcApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class File2MvcApplication {
 
 	public static void main(String[] args) {
-
+        System.out.println("Getting started....");
         SpringApplication.run(File2MvcApplication.class, args);
 	}
 }
